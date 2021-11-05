6bbcadd3d17dfe80d5163b0326f5498ecbe51bbe
Yeonggwang <immutable000@gmail.com>
2021-11-05 22:56:39 +0900

    solve: 9613 - GCD 합

--- 양영광/week3/[9613]GCD 합/9613.py
+++ 양영광/week3/[9613]GCD 합/9613.py
@@ -6,6 +6,26 @@
     난이도: Silver III
     태그: 유클리드 호제법, 수학, 정수론
 """
-import sys 
+import sys
 
 def input(): return sys.stdin.readline().rstrip()
+
+def GCD(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+for _ in range(int(input())):
+
+    nums = list(map(int, input().split()[1:]))
+
+    nums_count = len(nums)
+
+    answer = 0
+
+    # combination
+    for i in range(nums_count):
+        for j in range(i+1, nums_count):
+            answer += GCD(nums[i], nums[j])
+
+    print(answer)
--- 양영광/week3/[9613]GCD 합/README.md
+++ 양영광/week3/[9613]GCD 합/README.md
@@ -1,1 +1,30 @@
 # GCD 합
+
+## 정답 코드
+
+```python
+import sys
+
+def input(): return sys.stdin.readline().rstrip()
+
+def GCD(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+for _ in range(int(input())):
+
+    # 가장 앞 원소는 생략한다.
+    nums = list(map(int, input().split()[1:]))
+
+    nums_count = len(nums)
+
+    answer = 0
+
+    # combination
+    for i in range(nums_count):
+        for j in range(i+1, nums_count):
+            answer += GCD(nums[i], nums[j])
+
+    print(answer)
+```
